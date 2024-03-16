c002f9f72c091520746dbc8474b93e67a6e33488
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-03-16 15:44:15 -0400

    wried bug, num1 declared as global var but inside calc it's local & error

--- 09_calc_func/09_calc_func.py
+++ 09_calc_func/09_calc_func.py
@@ -2,4 +2,42 @@ import calc_art as art
 
 print(art.logo)
 
- 
+def add(n1, n2):
+   return n1 + n2
+def sub(n1, n2):
+   return n1 - n2
+def mult(n1, n2):
+   return n1 * n2
+def div(n1, n2):
+   return n1 / n2
+
+
+operations = {
+   "+" : add,
+   "-" : sub,
+   "*" : mult,
+   "/" : div
+}
+
+num1 = int(input("What's the first number?: "))
+for j in operations:
+   print(j)
+
+def calc():
+   while True:
+      operation_symbol = input("Pick an operation from the line above: ")
+      num2 = int(input("What's the next number?: "))
+
+      if operation_symbol in operations:
+         answer = operations[operation_symbol](num1,num2) # the reason we can access 'answer' b/c after we call the func() its returing something 
+
+      print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+      bool_yes_no = input("Type 'y' to continue calculating with 8, or type 'n' to exit.: ")
+      if bool_yes_no == "y":
+         num1 = answer
+         continue
+      else:
+         calc()
+         break
+calc()
