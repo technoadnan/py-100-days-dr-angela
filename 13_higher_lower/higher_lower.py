3435dae4efcdbec7bb8fac6b907eecff7153d6a3
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-03-25 23:14:43 -0400

    higher lower done

--- 13_higher_lower/higher_lower.py
+++ 13_higher_lower/higher_lower.py
@@ -3,22 +3,29 @@ from higher_lower_art import logo,vs
 from higher_lower_game_data import data
 print(logo)
 
+a = data[randint(0,len(data)-1)]
+a_follower = a['follower_count']
+b = data[randint(0,len(data)-1)]
+b_follower = b['follower_count']
+
+score = 0
 k = True
 while k:
-   score = 0
-   a,a_follower = data[randint(0,len(data)-1)],a['follower_count']
-   b,b_follower = data[randint(0,len(data)-1)],b['follower_count']
-
    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
    print(vs)
    print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}")
    user = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-   if user == "a" and a_follower > b_follower:
+   if (user == "a" and a_follower > b_follower) or (user == "b" and b_follower > a_follower):
       score += 1
       a=b
       a_follower=b_follower
-   else:
-      k = False
+      b = data[randint(0,len(data)-1)]
+      b_follower = b['follower_count']
+      if b == a:
+         b = data[randint(0,len(data)-1)]
+         b_follower = b['follower_count']
+      continue
+   k = False
 
-
+print("Sorry, that's wrong. Final score: ",score)
