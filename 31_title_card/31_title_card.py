254858168819b33091f91959404b2abd1e0feca5
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-05-04 23:25:51 -0400

    comp Fix bug in French flashcard script and update word_to_learn.csv

--- 31_title_card/31_title_card.py
+++ 31_title_card/31_title_card.py
@@ -1,82 +1,65 @@
-# ------------------------- IMPORTS ----------------------------- #
 import tkinter as tk
 import pandas as pd
-import random, time 
-# ------------------------- CONSTANTS -------------------------- #
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
-FONT_TITLE = ("Ariel",40, "italic")
-FONT_WORD = ("Ariel",60, "bold")
-all_words = pd.read_csv("data/french_words.csv")
-t = []
-fr_word = ""
-# -------------------------- pass ------------------------------ # 
-def swap():
-   global fr_word
-   fr_word = random.choice(all_words.French)
-   canvas.itemconfig(canvas_img,image=front_img)
-   title.config(text="French",background="#fff",foreground="#000")
-   word.config(text=fr_word,fg="white",borderwidth=0,highlightthickness=0,background="#fff",foreground="#000")
-   canvas.after(3000, right)  # Call right function after 3 seconds
-   # print("hello")
-def right():
-   en_word = all_words[all_words.French == fr_word].English.values[0]
-   # all_words = all_words.drop(all_words[all_words.French == fr_word].index)
-   canvas.itemconfig(canvas_img, image=back_img)
-   title.config(text="English", background="#91c2af",foreground="white")
-   word.config(text=en_word,background="#91c2af",foreground="white")
-   
-   
- 
-# -------------------------- keep ------------------------------#
-def wrong():
-   l = all_words["French"].tolist()
-   l.remove(fr_word)
-   e = all_words["English"].tolist()
-   q = all_words[all_words.French == fr_word].English.values[0]
-   e.remove(q)
-   with open("data/word_to_learn.csv", mode="a") as file:
-      file.write(f"{fr_word},{q}\n")
+FONT_TITLE = ("Ariel", 40, "italic")
+FONT_WORD = ("Ariel", 60, "bold")
+current_card = {}
+to_learn = {}
 
-   
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
-   swap()
-# ------------------------ UI --------------------------------- # 
+
+def next_card():
+    global current_card
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=front_img)
+    window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = tk.Tk()
 window.title("Flashy")
-window.config(padx=50,pady=50,bg=BACKGROUND_COLOR, width=800, height=526,border=0,highlightthickness=0)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = tk.Canvas(width=800, height=526,highlightthickness=0,borderwidth=0)
+canvas = tk.Canvas(width=800, height=526)
 front_img = tk.PhotoImage(file="images/card_front.png")
 back_img = tk.PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_img)
+card_title = canvas.create_text(400, 150, text="", font=FONT_TITLE)
+card_word = canvas.create_text(400, 263, text="", font=FONT_WORD)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
-canvas_img = canvas.create_image(400, 526/2, image=front_img)
-canvas.grid(column=0,row=0,columnspan=2)
+cross_image = tk.PhotoImage(file="images/wrong.png")
+unknown_button = tk.Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
 
-title = tk.Label(text=f"", font=FONT_TITLE,borderwidth=0,highlightthickness=0,background="#fff",foreground="#000")
-title.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+check_image = tk.PhotoImage(file="images/right.png")
+known_button = tk.Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
 
+next_card()
 
-word = tk.Label(text="",font=FONT_WORD)
-word.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
-
-swap()
-
-
-# # buttons
-right_img = tk.PhotoImage(file="images/right.png")
-wrong_img = tk.PhotoImage(file="images/wrong.png")
-
-btn_right = tk.Button(window, command=swap,highlightthickness=0,image=right_img,borderwidth=0, pady=30)
-btn_right.grid(column=0,row=1,pady=30)
-
-btn_wrong = tk.Button(text="Start",command=wrong,highlightthickness=0, image=wrong_img, borderwidth=0, pady=30)
-btn_wrong.grid(column=1,row=1, pady=30)
-
-# ---------------------- FILE ------------------------ # 
-with open("data/word_to_learn.csv", mode="r", encoding="ISO-8859-1") as file:
-   r = file.readlines()
-   print(r)
-   if len(r) > 1:
-      all_words = pd.read_csv("data/word_to_learn.csv", encoding="ISO-8859-1")
-# with open
 window.mainloop()
