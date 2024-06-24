7c5ce93b3c1faf4dd87275cf27c996342483ab0d
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-06-23 23:17:40 -0400

    Update Nutritionix script with correct API key variable name

--- 38_nutritionix/38_nutritionix.py
+++ 38_nutritionix/38_nutritionix.py
@@ -1,59 +1,59 @@
-# import requests
-# from datetime import datetime
+import requests
+from datetime import datetime
 
-# GENDER = "male"
-# WEIGHT_KG = 54
-# HEIGHT_CM = 167
-# AGE = 17
+GENDER = "male"
+WEIGHT_KG = 54
+HEIGHT_CM = 167
+AGE = 17
 
-# APP_ID = "fa375371"
-# API_KEY = "33782dcf2c07fd0d23e47fa80001b1bb"
+APP_ID = "fa375371"
+API_KEY = "xxx"
 
-# exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-# sheet_endpoint = "https://api.sheety.co/8c6e05b8c817e140544bf493cc9a0812/myworkouts/workouts"
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/8c6e05b8c817e140544bf493cc9a0812/myworkouts/workouts"
 
-# exercise_text = input("Tell me which exercises you did: ")
+exercise_text = input("Tell me which exercises you did: ")
 
-# headers = {
-#    "x-app-id": APP_ID,
-#    "x-app-key": API_KEY,
-# }
+headers = {
+   "x-app-id": APP_ID,
+   "x-app-key": API_KEY,
+}
 
-# parameters = {
-#    "query": exercise_text,
-#    "gender": GENDER,
-#    "weight_kg": WEIGHT_KG,
-#    "height_cm": HEIGHT_CM,
-#    "age": AGE
-# }
+parameters = {
+   "query": exercise_text,
+   "gender": GENDER,
+   "weight_kg": WEIGHT_KG,
+   "height_cm": HEIGHT_CM,
+   "age": AGE
+}
 
-# headers1 = {
-#    'Content-Type': 'application/json',
-#    "Authorization": "Bearer lkoyihkgmfbkgfjhklmgboprte"
-# }
+headers1 = {
+   'Content-Type': 'application/json',
+   "Authorization": "Bearer xxx"
+}
 
-# response = requests.post(exercise_endpoint, json=parameters, headers=headers)
-# result = response.json()
-# print(result)
-# ################### Start of Step 4 Solution ######################
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
+################### Start of Step 4 Solution ######################
 
-# today_date = datetime.now().strftime("%d/%m/%Y")
-# now_time = datetime.now().strftime("%X")
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
 
-# for exercise in result["exercises"]:
-#    sheet_inputs = {
-#       "workout": {
-#          "date": today_date,
-#          "time": now_time,
-#          "exercise": exercise["name"].title(),
-#          "duration": exercise["duration_min"],
-#          "calories": exercise["nf_calories"]
-#       }
-#    }
+for exercise in result["exercises"]:
+   sheet_inputs = {
+      "workout": {
+         "date": today_date,
+         "time": now_time,
+         "exercise": exercise["name"].title(),
+         "duration": exercise["duration_min"],
+         "calories": exercise["nf_calories"]
+      }
+   }
 
-#    sheet_response = requests.post(
-#       sheet_endpoint, json=sheet_inputs, headers=headers1)
-#    print(sheet_response.text)
+   sheet_response = requests.post(
+      sheet_endpoint, json=sheet_inputs, headers=headers1)
+   print(sheet_response.text)
 
 
 import requests
